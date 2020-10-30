851b394283a4525e826cfd00c86f1cdb84b46655
6Papagna <69843159+6Papagna@users.noreply.github.com>
2020-10-30 14:37:38 +0100

    Add files via upload

---
+++ sail.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gLXT7PZwPeRrz-nDqs8nfF5cxSLuSz2OpXp9AgGaibw/edit?ts=5f9bf122#gid=1033046795';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
